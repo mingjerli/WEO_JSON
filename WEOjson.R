3a1f739056d9d0837d7f1444dff99bdeaf155fe6
Mingjer Lee <mingjerli@gmail.com>
2015-07-01 17:33:26 -0400

    WEO json example

--- WEOjson.R
+++ WEOjson.R
@@ -6,22 +6,21 @@ rm(list=ls())
 library(jsonlite)
 download.file(url="http://www.imf.org/external/pubs/ft/weo/2015/01/weodata/WEOApr2015all.xls",destfile = "WEOApr2015all.xls")
 WEO = read.delim("WEOApr2015all.xls",header = TRUE, nrows = 8316, stringsAsFactors = FALSE)
-WEOdata=WEO
+WEOtidy=WEO
 
-for(i in 1:length(WEOdata)){
+for(i in 1:length(WEOtidy)){
   print(i)
-  WEOdata[which(grepl("n[:/:]a",WEOdata[,i])),i] = ""
-  WEOdata[which(grepl("--",WEOdata[,i])),i] = ""
+  WEOtidy[which(grepl("n[:/:]a",WEOtidy[,i])),i] = ""
+  WEOtidy[which(grepl("--",WEOtidy[,i])),i] = ""
 }
-for(i in 10:length(WEOdata)){
-  WEOdata[,i] = as.numeric(gsub(",","",WEOdata[,i]))
+for(i in 10:length(WEOtidy)){
+  WEOtidy[,i] = as.numeric(gsub(",","",WEOtidy[,i]))
 }
 
-# Some Examples
-toJSONsubset = function(df, rows, cols, ...){
-  toJSON(df[rows, cols], ...)
-}
-toJSONsubset(WEOdata, 1:6, 31:40, pretty=TRUE)
-toJSONsubset(WEO, 1:6, 31:40, pretty=TRUE)
-toJSONsubset(WEOdata, which(WEOdata$WEO.Country.Code=="512"), c(1:5,21:40), pretty=TRUE)
-toJSONsubset(WEO, which(WEOdata$WEO.Country.Code=="512"), c(1:5,21:40), pretty=TRUE)
+# Some Examples for comparison
+toJSON(WEOtidy[1:6, 31:40], pretty=TRUE)
+toJSON(WEO[1:6, 31:40], pretty=TRUE)
+toJSON(WEOtidy[which(WEOtidy$WEO.Country.Code=="512"), c(1:5,21:40)], pretty=TRUE)
+toJSON(WEO[which(WEO$WEO.Country.Code=="512"), c(1:5,21:40)], pretty=TRUE)
+WEO_json = toJSON(WEO, pretty=TRUE)
+WEOtidy_json = toJSON(WEOtidy, pretty=TRUE)
